982f74813ea6273fc61c68e258a90bd770bd25dc
alok-07-pandey <alok00007pandey@gmail.com>
2025-06-19 12:29:31 +0530

    fix: Spelling MIstake

--- netflix/src/components/MovieContainer.js
+++ netflix/src/components/MovieContainer.js
@@ -12,7 +12,7 @@ const MovieContainer = () => {
       <div className='-mt-52 relative z-10' >
         <MovieList title={"Popular Movies"} movies={movie.popularMovie}/>
         <MovieList title={"Now Playing Movies"} movies={movie.nowPlayingMovies}/>
-        <MovieList title={"Top Tated Movies"} movies={movie.topRatedMovies}/>
+        <MovieList title={"Top Rated Movies"} movies={movie.topRatedMovies}/>
         <MovieList title={"Upcoming Movies"} movies={movie.upcomingMovies}/>
       </div>
     </div>
